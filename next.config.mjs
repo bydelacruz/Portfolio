84e031d5b57cfdae1ba4aefb6c711f0e69eabddd
Benjamin Delacruz <bydelacruz@icloud.com>
2025-09-20 21:20:39 -0400

    updated deploy yml file to include some debugging prints, fix missing dependency issue in eslint config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,18 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = { typedRoutes: true };
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+       '@': path.resolve(__dirname),         
+    };
+    return config;
+  },
+};
+
 export default nextConfig;
